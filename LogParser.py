fb37ade74c013aee01f2363cef887002aa85935b
Paul Hooijenga <paulhooijenga@gmail.com>
2011-11-07 18:29:58 +0000

    - Rewritten parsers

--- LogParser.py
+++ LogParser.py
@@ -1,103 +1,94 @@
 import re
 import time
 
-class LogParser(object):
-	IGNORE = "IGNORE"
-	messageParsers = []
+LINE_REGEXES = {
+	"Apache Error Log": {
+		"regex": re.compile(r"^\[([^\]]+)\] \[([^\]]+)\](?: \[client ([^\]]+)\])? (.*?)(?:, referer: (.*))?$"),
+		"groups": ("date", "level", "client", "message", "referer"),
+		"dateFormat": "%a %b %d %H:%M:%S %Y", # Sun Oct 24 06:48:26 2010
+		"parseMessage": True
+	},
+	"Bebras Access Log": {
+		# LogFormat "%{Host}i %h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" beaver_combined
+		"regex": re.compile(r"^([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) \[([^\]]+) +\d\d\d\d\] \"([^\"]+)\" \d+ (\d+|-) \"([^\"]+)\" \"([^\"]+)\"$"),
+		"groups": ("host", "client", "logname", "user", "date", "request", "status", "bytes", "referer", "ua"),
+		"dateFormat": "%d/%b/%Y:%H:%M:%S"
+	},
+	"Apache Access Log (combined)": {
+		# LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
+		"regex": re.compile(r"$([^ ]+) ([^ ]+) ([^ ]+) \[([^\]]+) +\d\d\d\d\] \"([^\"]+)\" \d+ (\d+|-) \"([^\"]+)\" \"([^\"]+)\"$"),
+		"groups": ("client", "logname", "user", "date", "request", "status", "bytes", "referer", "ua"),
+		"dateFormat": "%d/%b/%Y:%H:%M:%S"
+	}
+}
 
-	def __init__(self):
-		pass
+MESSAGE_IGNORE = [
+	re.compile("^PHP Stack trace:$"),
+	re.compile(r"^PHP [ 0-9][ 0-9][ 0-9]\. .*$"),
+]
 
-	@staticmethod
-	def addMessageParser(mp):
-		LogParser.messageParsers.append(mp)
+MESSAGE_REGEXES = {
+	"PHP Error": [
+		{
+			"regex": re.compile(r"^PHP (Fatal error|Parse error|Warning|Notice|Deprecated):  (.*?)(?: in (?!.*? in )(/var/www/.*?) on line (\d+))?$"),
+			"groups": ("level", "message", "file", "line")
+		}
+	],
+	"File Not Found": [
+		{
+			"regex": re.compile(r"^File does not exist: (.*)$"),
+			"groups": ("file", )
+		},
+		{
+			"regex": re.compile(r"^script '([^']+)' not found or unable to stat$"),
+			"groups": ("file", )
+		}
+	],
+	"Apache": [
+		{ "regex": re.compile(r"^caught SIGTERM, shutting down$") },
+		{ "regex": re.compile(r"^Graceful restart requested, doing restart$") }
+	]
+}
 
-	@staticmethod
-	def parseMessage(message):
-		for mp in LogParser.messageParsers:
-			parsed = mp.parseLine(message)
-			if parsed != None:
-				return mp.TYPE, parsed
-		return None, None
-	
-	def parseLine(self, line):
-		return None
-	
-	@staticmethod
-	def parseLineRegex(line, regex, groupNames):
-		match = regex.match(line)
+def groupsToDict(match, groupNames):
+	groups = match.groups()
+	return dict(zip(groupNames, groups))
+
+def parseLine(line):
+	for name, opts in LINE_REGEXES.iteritems():
+		match = opts["regex"].match(line)
 		if not match:
-			return None
-		groups = match.groups()
-		if len(groups) != len(groupNames):
-			raise Exception("Unable to parse line %r. Unexpected group count." % line)
-		return dict(zip(groupNames, groups))
+			continue
 
-class ApacheErrorLogParser(LogParser):
-	LOG = re.compile(r"^\[([^\]]+)\] \[([^\]]+)\](?: \[client ([^\]]+)\])? (.*?)(?:, referer: (.*))?$")
-	GROUPS = ("date", "level", "client", "message", "referer")
-	DATE = "%a %b %d %H:%M:%S %Y" # Sun Oct 24 06:48:26 2010
+		parsed = groupsToDict(match, opts["groups"])
+		if "dateFormat" in opts and "date" in parsed:
+			parsed["date"] = time.strptime(parsed["date"], opts["dateFormat"])
+		if "parseMessage" in opts and "message" in parsed:
+			messageType, messageParsed = parseMessage(parsed["message"])
+			if messageType == IGNORE:
+				return None
+			parsed["messageType"] = messageType
+			parsed["messageParsed"] = messageParsed
+		return parsed
 
-	def parseLine(self, line):
-		parseDict = LogParser.parseLineRegex(line, self.LOG, self.GROUPS)
-		if parseDict == None:
-			print "Unable to parse %r" % line
-			return None
+	return None
 
-		parseDict["date"] = time.strptime(parseDict["date"], self.DATE)
+IGNORE = 1
+def parseMessage(message):
+	for regex in MESSAGE_IGNORE:
+		if regex.match(message):
+			return IGNORE, None
+	
+	for key, allOpts in MESSAGE_REGEXES.iteritems():
+		for opts in allOpts:
+			match = opts["regex"].match(message)
+			if not match:
+				continue
 
-		parser, parsed = LogParser.parseMessage(parseDict["message"])
-		if parser == LogParser.IGNORE:
-			return None
-		parseDict["message_type"] = parser
-		parseDict["message_parsed"] = parsed
+			if "groups" in opts:
+				parsed = groupsToDict(match, opts["groups"])
+			else:
+				parsed = { "message": message }
+			return key, parsed
 
-		return parseDict
-
-class PHPErrorParser(LogParser):
-	TYPE = "PHP Error"
-	# Yes, that is two spaces between the colon and the message...
-	LINE = re.compile(r"^PHP (Fatal error|Parse error|Warning|Notice|Deprecated):  (.*?)(?: in (?!.*? in )(/var/www/.*?) on line (\d+))?$")
-	GROUPS = ("level", "message", "file", "line")
-
-	@staticmethod
-	def parseLine(line):
-		return LogParser.parseLineRegex(line, PHPErrorParser.LINE, PHPErrorParser.GROUPS)
-LogParser.addMessageParser(PHPErrorParser)
-
-class PHPIgnore(LogParser):
-	TYPE = LogParser.IGNORE
-	LINE = re.compile(r"^PHP [ 0-9][ 0-9][ 0-9]\. .*$")
-
-	@staticmethod
-	def parseLine(line):
-		if line == "PHP Stack trace:" or PHPIgnore.LINE.match(line):
-			return True
-LogParser.addMessageParser(PHPIgnore)
-
-class FileNotFoundParser(LogParser):
-	TYPE = "File Not Found"
-	LINE = [re.compile(r"^File does not exist: (.*)$"), re.compile(r"^script '([^']+)' not found or unable to stat$")]
-	GROUPS = ("file", )
-
-	@staticmethod
-	def parseLine(line):
-		for regex in FileNotFoundParser.LINE:
-			parsed = LogParser.parseLineRegex(line, regex, FileNotFoundParser.GROUPS)
-			if parsed != None:
-				return parsed
-		return None
-LogParser.addMessageParser(FileNotFoundParser)
-
-class ApacheLifeCycleParser(LogParser):
-	TYPE = "Apache"
-
-	@staticmethod
-	def parseLine(line):
-		if line == "caught SIGTERM, shutting down":
-			return {"type": "shutdown", "message": line}
-		if (line.endswith("-- resuming normal operations") or 
-			line == "Graceful restart requested, doing restart"):
-			return {"type": "restart", "message": line}
-		return None
-LogParser.addMessageParser(ApacheLifeCycleParser)
+	return None, None
